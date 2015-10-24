f8f5cc9594469f1b290672f4ffa3b16018b91193
Flemming Thesbjerg <flemming.thesbjerg@gmail.com>
2015-10-24 21:11:34 +0200

    custom launchagents

--- symlinks.sh
+++ symlinks.sh
@@ -4,30 +4,36 @@ dir=~/.dotfiles
 olddir=$dir/tmp
 verbose=false
 launchagentsPath=$dir/launchagents
+vundleGit=https://github.com/VundleVim/Vundle.vim.git
+vundlePath=~/.vim/bundle/Vundle.vim
 
 mkdir -p $olddir
 
-if ! git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim &> /dev/null
+if ! git clone $vundleGit $vundlePath &>-
 then
   $verbose && echo "vundle already installed. Moving on"
 fi
 
-for file in $(ls $dir/launchagents)
+for file in $(ls $launchagentsPath)
 do
-  ln -s $launchagentsPath ~/Library/LaunchAgents/$file
-  launchctl load ~/Library/LaunchAgents/$file
+  if ! ln -s $launchagentsPath/$file ~/Library/LaunchAgents/$file &>-
+  then
+    $verbose && echo "$file already symlinked"
+  else
+    launchctl load ~/Library/LaunchAgents/$file
+  fi
 done
 
 for file in $(ls $dir/scripts)
 do
-  mv -f ~/.$file $olddir &> /dev/null
-  if ! ln -s $dir/scripts/$file ~/.$file &> /dev/null; then
+  mv -f ~/.$file $olddir &>-
+  if ! ln -s $dir/scripts/$file ~/.$file &>-; then
     $verbose && echo "$file already symlinked"
   fi
 done
 
 for vimfile in $(ls $dir/vimfiles); do
-  if ! ln -s $dir/vimfiles/$vimfile ~/.vim/.$vimfile &> /dev/null; then
+  if ! ln -s $dir/vimfiles/$vimfile ~/.vim/.$vimfile &>-; then
     $verbose && echo "$vimfile already symlinked"
   fi
 done
