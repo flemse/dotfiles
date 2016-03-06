6e3318f11dd48ca630c3826216cf235e1c2fa2d7
Flemming Thesbjerg <flemming.thesbjerg@gmail.com>
2016-03-06 20:02:31 +0100

    avoid garbage output to file

--- symlinks.sh
+++ symlinks.sh
@@ -9,14 +9,14 @@ vundlePath=~/.vim/bundle/Vundle.vim
 
 mkdir -p $olddir
 
-if ! git clone $vundleGit $vundlePath &>-
+if ! git clone $vundleGit $vundlePath > /dev/null 2>&1
 then
   $verbose && echo "vundle already installed. Moving on"
 fi
 
 for file in $(ls $launchagentsPath)
 do
-  if ! ln -s $launchagentsPath/$file ~/Library/LaunchAgents/$file &>-
+  if ! ln -s $launchagentsPath/$file ~/Library/LaunchAgents/$file > /dev/null 2>&1
   then
     $verbose && echo "$file already symlinked"
   else
@@ -26,14 +26,14 @@ done
 
 for file in $(ls $dir/scripts)
 do
-  mv -f ~/.$file $olddir &>-
-  if ! ln -s $dir/scripts/$file ~/.$file &>-; then
+  mv -f ~/.$file $olddir  > /dev/null 2>&1
+  if ! ln -s $dir/scripts/$file ~/.$file > /dev/null 2>&1; then
     $verbose && echo "$file already symlinked"
   fi
 done
 
 for vimfile in $(ls $dir/vimfiles); do
-  if ! ln -s $dir/vimfiles/$vimfile ~/.vim/.$vimfile &>-; then
+  if ! ln -s $dir/vimfiles/$vimfile ~/.vim/.$vimfile > /dev/null 2>&1; then
     $verbose && echo "$vimfile already symlinked"
   fi
 done
